d2482600b78e4b5a2f3aeb1cc29531d905be90e1
Sava Max Heron <12586876+SavaHeron@users.noreply.github.com>
2020-01-19 11:11:50 +0000

    Update dns.js

--- dns.js
+++ dns.js
@@ -165,9 +165,9 @@ class Dnsserver {
             } else if (typeof cache != `undefined`) {   //if the dns server has already cached the domain's ip
                 console.log(cache.retrieved);   //time at which retreived
                 console.log(cache[1])
-                let now = new Date();
-                let then = cache.retrieved;
-                let thenplusttl = dt.addSeconds(then, +cache[1]);
+                let now = new Date().getTime();
+                let then = new Date(cache.retrieved).getTime();
+                //let thenplusttl = dt.addSeconds(then, +cache[1]);
                 console.log(thenplusttl)
                 if (false) {    //if the record is valid
                     var valid = 1
