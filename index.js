cdbaea5f931c8977fde34ac0752dd0d7e96c5da0
Sava Max Heron <12586876+SavaHeron@users.noreply.github.com>
2020-01-23 10:42:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ Licence:	CC BY-NC-ND 4.0
 
 const Dnsserver = require (`./dns`);
 //const App = require(`./admin`);
-//const setup = require(`./setup`);
+const setup = require(`./setup`);
 const ip = require(`ip`);
 
 const localip = ip.address();
+console.log(localip);
 
 const externalresolver1 = {
 	address: `1.1.1.1`,
@@ -33,8 +34,8 @@ const externalresolver2 = {
 /*const app1 = new App(appport);
 app1.startapp();*/
 
-const dns1 = new Dnsserver(localip, 53,  externalresolver1);
-dns1.startserver();
+//const dns1 = new Dnsserver(localip, 53,  externalresolver1);
+//dns1.startserver();
 
-/*const dns2 = new Server(localip, 53, externalresolver2);
-dns2.startserver();*/
+//const dns2 = new Server(, 53, externalresolver2);
+//dns2.startserver();
