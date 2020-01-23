2ddaf1a28219d1b84d5d7229e908d6a74f7350e4
Sava Max Heron <12586876+SavaHeron@users.noreply.github.com>
2020-01-23 10:44:41 +0000

    Create dhcp.js

---
+++ dhcp.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+Name: 		dhcp.js
+Purpose:	A-Level Computer Science coursework DHCP script
+Author:		Sava Max Heron
+Created:	23/01/2020
+Copyright:	(c) SMH 2020
+Licence:	CC BY-NC-ND 4.0
+*/
+
