6eabfd60a0101aec524de560890de905415e3ea3
Sava Max Heron <12586876+SavaHeron@users.noreply.github.com>
2020-02-06 09:31:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ Licence:	CC BY-NC-ND 4.0
 //const mariadb = require(`mariadb`);
 //const util = require(`util`);
 //const exec = util.promisify(require(`child_process`).exec);
-const Dnsserver = require(`./dns`);
-const Dhcpserver = require(`./dhcp`);
+const DNSServer = require(`./dns`);
+const DHCPServer = require(`./dhcp`);
 //const App = require(`./admin`);
 
 const dbuser = `root`;
@@ -21,16 +21,17 @@ const dbpassword = `9a_?KedofR-qewo`;
 const localip = `10.0.0.1`;
 const resolver = `1.1.1.1`;
 const apikey = `b8187ab8-b907-4a0f-a647-f7e508ee0ce7`;
+const dns = new DNSServer(dbuser, dbpassword, localip, resolver, apikey);
 
 const beginrange = `192.168.3.10`;
 const endrange = `192.168.3.99`;
 const netmask = '255.0.0.0';
+const dhcp = new DHCPServer(beginrange, endrange, netmask, localip);
 
-const dns = new Dnsserver(dbuser, dbpassword, localip, resolver, apikey);
-dns.startserver();
-
-const dhcp = new Dhcpserver(beginrange, endrange, netmask, localip);
-dhcp.startserver();
+//start() {
+    dns.startserver();
+    dhcp.startserver();
+//};
 
 /*const app1 = new App(localip);
 app1.startapp();*/
