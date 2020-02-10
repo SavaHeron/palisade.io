48cef32d4b787108e82993eccdd9519c428f4448
Sava Max Heron <12586876+SavaHeron@users.noreply.github.com>
2020-02-10 15:41:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ Licence:	CC BY-NC-ND 4.0
 */
 
 //const mariadb = require(`mariadb`);
-//const util = require(`util`);
-//const exec = util.promisify(require(`child_process`).exec);
+const util = require(`util`);
+const exec = util.promisify(require(`child_process`).exec);
 const DNSServer = require(`./dns`);
 const DHCPServer = require(`./dhcp`);
 const admin = require(`./admin`);
@@ -26,12 +26,49 @@ const dns = new DNSServer(dbuser, dbpassword, localip, resolver, apikey);
 const beginrange = `192.168.3.10`;
 const endrange = `192.168.3.99`;
 const netmask = '255.0.0.0';
+const broadcast = `10.255.255.255`;
 const dhcp = new DHCPServer(beginrange, endrange, netmask, localip);
 
-//start() {
+async function setnameserver(resolver) {
+    try {
+        const { stdout, stderr } = await exec(`echo nameserver ${resolver} | sudo tee /etc/resolv.conf`);
+        console.log(`stdout:`, stdout);
+        console.log(`stderr:`, stderr);
+    } catch (error) {
+        console.error(err);
+    };
+};
+
+async function setnat() {
+    try {
+        const { stdout, stderr } = await exec(`sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE`);
+        console.log(`stdout:`, stdout);
+        console.log(`stderr:`, stderr);
+    } catch (error) {
+        console.error(err);
+    };
+};
+
+async function setstaticip(localip) {
+    try {
+        const { stdout, stderr } = await exec(`sudo ip addr add ${localip}/8 dev eth0 broadcast ${broadcast} | sudo route add -host 255.255.255.255 eth0`);
+        console.log(`stdout:`, stdout);
+        console.log(`stderr:`, stderr);
+    } catch (error) {
+        console.error(err);
+    };
+};
+
+
+function start() {
+    setnameserver(resolver);
+    setnat();
+    setstaticip(localip);
     dns.startserver();
     dhcp.startserver();
-//};
+};
 
 /*const app1 = new App(localip);
 app1.startapp();*/
+
+start();
